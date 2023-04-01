a463a94f50605ee7c3d1f17bcdd636743519e752
karlozizic <karlo.zizic1@gmail.com>
2023-04-01 12:58:39 +0200

    inicijalizacija react frontenda

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EkoRazred from './components/Ekorazred';
+
+function App() {
+  return (
+    <div className='App'>
+      <EkoRazred/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Ekorazred.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Ekorazred(){
+    const {id, naziv} = props.Ekorazred; 
+
+    return(
+    <p>Eko razred 1</p>
+    );
+}
+
+export default Ekorazred;
