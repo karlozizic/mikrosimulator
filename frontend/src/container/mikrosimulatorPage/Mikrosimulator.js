c5ba195a0a4e9ce46f99c4f69e9c5981d25736d9
karlozizic <karlo.zizic1@gmail.com>
2023-06-17 13:54:54 +0200

    prije promjene u app.js

--- frontend/src/container/mikrosimulatorPage/Mikrosimulator.js
+++ frontend/src/container/mikrosimulatorPage/Mikrosimulator.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {useEffect, useState} from 'react';
 import {Button, Card, CardBody, Col, Container, Form, FormGroup, Row, Table, Input, Label} from 'reactstrap';
 import Select from "react-select";
+import axios from "axios";
 //import css file
 
 const Mikrosimulator = () => {
+    const [brojVozila, setBrojVozila] = useState('');
+
+    const generateOcitanje = () => {
+        //samo GET request
+        axios.get('http://localhost:4444/generate')
+    };
+
+    const generateVozila = () => {
+        axios.get('http://localhost:2222/vehicles/generate/' + brojVozila)
+    };
+
+    const changeHandler = (event) => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'brojVozila':
+                setBrojVozila(value);
+                break;
+            default:
+                break;
+        }
+    }
 
     return (
         <div>
@@ -14,58 +37,20 @@ const Mikrosimulator = () => {
                             <h3>Mikrosimulator</h3>
                             <CardBody>
                                 <Label>Broj vozila</Label>
-                                <Input></Input>
+                                <Input name={"brojVozila"} onChange={changeHandler}></Input>
                                 <Label>Vremenski interval</Label>
                                 <Label>Od</Label>
                                 <Input></Input>
                                 <Label>Do</Label>
                                 <Input></Input>
-                                <Button>Generiraj Vozila</Button>
-                                <a href="http://localhost:3333/vehicles/all">
-                                    <Button>Pregled Vozila</Button>
-                                </a>
-                                <a href="http://localhost:4444/generate">
-                                    <Button>Generiraj Očitanja</Button>
-                                </a>
+                                <Button onClick={generateVozila}>Generiraj Vozila</Button>
+                                <Button>Pregled Vozila</Button>
+                                <div>
+                                    <Button onClick={generateOcitanje}>Generiraj Očitanja</Button>
+                                </div>
                                 <a href="http://localhost:5555/payments/all">
                                     <Button>Pregled Očitanja</Button>
                                 </a>
-                                {/*<Form>*/}
-                                    {/*<FormGroup style={{ padding: '1em' }}>*/}
-                                    {/*    <label>Smjer:</label>*/}
-                                    {/*    <input name="smjer" className="form-control" value={smjer === "" ? null : smjer} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Najveca brzina:</label>*/}
-                                    {/*    <input name="najvecaBrzina" className="form-control" value={najvecaBrzina === "" ? null : najvecaBrzina} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Broj traka:</label>*/}
-                                    {/*    <input name="brojTraka" className="form-control" value={brojTraka === "" ? null : brojTraka} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Oznaka:</label>*/}
-                                    {/*    <input name="oznaka" className="form-control" value={oznaka === "" ? null : oznaka} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Pocetna stacionaza:</label>*/}
-                                    {/*    <input name="pocetnaStacionaza" className="form-control" value={pocetnaStacionaza ==="" ? null : pocetnaStacionaza} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Zavrsna stacionaza:</label>*/}
-                                    {/*    <input name="zavrsnaStacionaza" className="form-control" value={zavrsnaStacionaza === "" ? null : zavrsnaStacionaza} onChange={changeHandler}></input>*/}
-                                    {/*    <label>Oznaka autoceste:</label>*/}
-                                    {/*    <input name="oznakaAutoceste" className="form-control" value={oznakaAutoceste === "" ? null : oznakaAutoceste} onChange={changeHandler}></input>*/}
-                                    {/*    Dionica Prije:*/}
-                                    {/*    <Select*/}
-                                    {/*        name="dionicaPrije"*/}
-                                    {/*        value={dionicaPrije}*/}
-                                    {/*        onChange={handleDionicaPrijeChange}*/}
-                                    {/*        options={dionice && dionice.length > 0 ? dionice.map((dionica) => ({ value: dionica.dionicaId, label: dionica.oznaka })) : []}/>*/}
-                                    {/*    Dionica poslije*/}
-                                    {/*    <Select*/}
-                                    {/*        name="dionicaPoslije"*/}
-                                    {/*        value={dionicaPoslije}*/}
-                                    {/*        onChange={handleDionicaPoslijeChange}*/}
-                                    {/*        options={dionice && dionice.length > 0 ? dionice.map((dionica) => ({ value: dionica.dionicaId, label: dionica.oznaka })) : []}/>*/}
-                                    {/*</FormGroup>*/}
-                                    {/*<Button color="success" onClick={saveDionica}>*/}
-                                    {/*    Save*/}
-                                    {/*</Button>*/}
-                                    {/*<Button style={{marginLeft: "1em"}} color="danger" onClick={cancel}>*/}
-                                    {/*    Cancel*/}
-                                    {/*</Button>*/}
-                                {/*</Form>*/}
                             </CardBody>
                         </Col>
                     </Card>
